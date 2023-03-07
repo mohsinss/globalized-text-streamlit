810943f52669877ba482c247ea113e1e7f8c5885
Mohsin <92655551+mohsinss@users.noreply.github.com>
2023-03-06 21:49:47 -0800

    direct tone added

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
 import streamlit as st
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+template = """
+    Below is an email that may be poorly worded.
+    Your goal is to:
+    - Properly format the email
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
+
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+    - Direct: Went to Barcelona for the weekend.  
+
+    
+    Here are some examples of words in different dialects:
+    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
+    - Direct: stern, straight, quickest, unbroken, nonstop, uninterrupted, to the point, no emotions, straight through
+
+    Example Sentences from each dialect:
+    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+    - Direct:I went to the produce section to get fresh vegetables such as bell peppers and zucchini. Then, I went to the meat department to grab some chicken breasts.
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+    
+    YOUR {dialect} RESPONSE:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["tone", "dialect", "email"],
+    template=template,
+)
+
+def load_LLM():
+    """Logic for loading the chain you want to use should go here."""
+    # Make sure your openai_api_key is set as an environment variable
+    llm = OpenAI(temperature=.7)
+    return llm
+
+llm = load_LLM()
 
 st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
 st.header("Globalize Text")
 
-col1, col2, col3= st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
@@ -12,10 +65,45 @@ with col1:
                 [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
 
 with col2:
-    st.write("hello world")
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
-    
-with col3:
     st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
 
-input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+st.markdown("## Enter Your Email To Convert")
+
+
+
+col1, col2 = st.columns(2)
+with col1:
+    option_tone = st.selectbox(
+        'Which tone would you like your email to have?',
+        ('Formal', 'Informal', 'Direct'))
+    
+with col2:
+    option_dialect = st.selectbox(
+        'Which English Dialect would you like?',
+        ('American', 'British', 'Direct'))
+
+def get_text():
+    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    return input_text
+
+email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter email. The maximum length is 700 words.")
+    st.stop()
+
+def update_text_with_example():
+    print ("in updated")
+    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+
+st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+
+st.markdown("### Your Converted Email:")
+
+if email_input:
+    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+
+    formatted_email = llm(prompt_with_email)
+
+    st.write(formatted_email)
+    
